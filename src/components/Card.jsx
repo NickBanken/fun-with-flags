b93cfc17478eb8da4fd01ad38127694b4d392d87
NickBanken <nick.banken@gmail.com>
2022-05-27 16:04:16 +0200

    replaced svg with png

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ const Card = ({country}) =>{
 
         <Link to={{pathname:`/country/${country.name.common}`}}>
             <div className={"sm:h-vw-20 md:h-vw-15 lg:h-vw-10 h-vw-50 "}>
-                <img className={"object-cover w-full h-full"} src={country.flags.svg} alt=""/>
+                <img className={"object-cover w-full h-full"} src={country.flags.png} alt=""/>
             </div>
             <div className={"p-5 mb-10"}>
                 <h3 className={"font-black text-lg mb-5 line-clamp-1"}>{country.name.common}</h3>
